fb8286639886f99e8efd11c50170f33e8ef13b83
Zecbmo Mc <gmccartan05@gmail.com>
2018-01-26 21:36:43 +0000

    merging basics

---
+++ Assets/Scripts/Towers/Tower.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This is a tower object that will be placed into the scene. 
+/// Players will be able to collect water/mineral from these towers
+/// </summary>
+public class Tower : MonoBehaviour {
+
+
+    [SerializeField, Range(0,100)]
+    float percentageFilled = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    float removeFluid(float removalSpeed)
+    {
+        float amountRemoved = 0;
+
+
+        return amountRemoved;
+    }
+}
