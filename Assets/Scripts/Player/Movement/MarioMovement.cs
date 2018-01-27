3ef7765990efe60839805633032c9c1729d2a864
Rubén Osorio López <rubeninhofonsa@gmail.com>
2018-01-27 10:45:23 +0000

    Added sprinting and looking direction

--- Assets/Scripts/Player/Movement/MarioMovement.cs
+++ Assets/Scripts/Player/Movement/MarioMovement.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using UnityEngine.Assertions;
 using UnityEngine;
 
+/// <summary>
+/// 
+/// Component for a modified Super Mario like movement
+/// for a 2D plattformer uning Unity's 2D physics system.
+/// 
+/// </summary>
 public class MarioMovement : MonoBehaviour
 {
 
-    [SerializeField]
-    private LayerMask groundedLayerMask;
 
     [Header("Horizontal movement parameters")]
     [SerializeField, Range(0, 500)]
     private float movementSpeed = 1.0f;
 
+    private bool switchedDirections = false;
+    private bool lookingRight = true;
+
+
+    [Header("Jumping parameters")]
     [SerializeField, Range(0, 500)]
     private float initialJumpForce = 0.0f;
 
@@ -28,12 +37,29 @@ public class MarioMovement : MonoBehaviour
     [SerializeField, Range(1, 5)]
     private float gravityMultiplier = 2.0f;
 
-    
-
     private bool jumping = false;
     private bool jumped = false;
     private float jumpTimestamp = -1.0f;
     private float originalGravity = 1.0f;
+    private bool grounded = false;
+
+
+    [Header("Sprinting parameters")]
+    [SerializeField, Range(1, 3)]
+    private float sprintMultiplier = 1.3f;
+    [SerializeField, Range(0, 0.5f)]
+    private float sprintDelay = 0.3f;
+
+    private float sprintTimeStamp = -1.0f;
+    private bool jumpedOnSprint = false;
+
+
+
+    [Header("Other parameters")]
+    [SerializeField]
+    private LayerMask groundedLayerMask;
+
+
 
 
     /// <summary>
@@ -60,9 +86,9 @@ public class MarioMovement : MonoBehaviour
 
     private void Update()
     {
-        bool grounded = IsGrounded();
         bool jumpButtonDown = Input.GetButtonDown("Jump");
         bool jumpButtonUp = Input.GetButtonUp("Jump");
+        bool pressedHorizontal = Input.GetButtonDown("Horizontal");
 
 
         if (jumpButtonDown && grounded)
@@ -76,18 +102,53 @@ public class MarioMovement : MonoBehaviour
         {
             jumping = false;
         }
-        
+
+
+        if (pressedHorizontal || switchedDirections)
+        {
+            sprintTimeStamp = Time.time;
+            jumpedOnSprint = false;
+        }
+
+        switchedDirections = false;
+
     }
 
     private void FixedUpdate()
     {
+        grounded = IsGrounded();
 
         float horizontalInput = Input.GetAxis("Horizontal");
 
         /*
-        We input the horizontal speed that we desire. 
+        We input the horizontal speed that we desire based
+        on the state of movement we are in
          */
-        rigidBody.velocity = new Vector2(horizontalInput * movementSpeed * Time.deltaTime, rigidBody.velocity.y);
+        if (Input.GetButton("Sprint") &&
+            Time.time - sprintTimeStamp > sprintDelay &&
+            grounded || jumpedOnSprint)
+        {
+            rigidBody.velocity = new Vector2(horizontalInput * movementSpeed * sprintMultiplier * Time.deltaTime, rigidBody.velocity.y);
+
+            if (jumped)
+            {
+                jumpedOnSprint = true;
+            }
+        }
+        else
+        {
+            rigidBody.velocity = new Vector2(horizontalInput * movementSpeed * Time.deltaTime, rigidBody.velocity.y);
+        }
+
+
+        bool currentLookingRight = (rigidBody.velocity.x > 0);
+        if (currentLookingRight != lookingRight)
+        {
+            switchedDirections = true;
+            lookingRight = currentLookingRight;
+        }
+
+
 
         /*
          We add the initial jumping force, this is the
@@ -115,10 +176,16 @@ public class MarioMovement : MonoBehaviour
         {
             rigidBody.gravityScale = originalGravity * gravityMultiplier;
         }
-        else {
+        else
+        {
             rigidBody.gravityScale = originalGravity;
         }
 
+    }
+
+    public bool IsLookingRight()
+    {
+        return lookingRight;
     }
 
     public bool IsGrounded()
@@ -140,6 +207,39 @@ public class MarioMovement : MonoBehaviour
 
     }
 
+    public bool IsTouchingWall()
+    {
+        /*
+         We check if a square from the center of our body to
+         our "face" is "touching" the another collider that we can
+         consider a wall.
+         */
 
+        //@@DOING
+        return Physics2D.OverlapArea(
+            new Vector3(boxCollider.bounds.min.x,
+                boxCollider.bounds.center.y,
+                boxCollider.bounds.center.z),
+            new Vector3(boxCollider.bounds.max.x,
+                boxCollider.bounds.min.y - 0.1f,
+                boxCollider.bounds.center.z),
+            groundedLayerMask);
+
+    }
+
+    /*
+     @@TODO: If there is time.
+     */
+
+
+    public void StartSliding()
+    {
+
+    }
+
+    public void EndSliding()
+    {
+
+    }
 
 }
