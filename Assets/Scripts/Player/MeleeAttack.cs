c79b269dc8bbcc86f8cf3068be1d87f4b08790eb
Pablo Tomico Pérez <pablotomico@hotmail.com>
2018-01-27 00:41:38 +0000

    Implemented melee attack mechanic

---
+++ Assets/Scripts/Player/MeleeAttack.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class MeleeAttack : MonoBehaviour
+{
+
+    [SerializeField]
+    float meleeAttackDuration;
+
+    Collider2D meleeAttackCollider;
+
+    void Awake()
+    {
+        meleeAttackCollider = GetComponent<Collider2D>();
+        Assert.IsNotNull(meleeAttackCollider);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            PerformMeleeAttack();
+        }
+    }
+
+    void PerformMeleeAttack()
+    {
+        StartCoroutine(MeleeAttackCoroutine());
+    }
+
+    IEnumerator MeleeAttackCoroutine()
+    {
+        float startTime = Time.time; // Change this if we use custom Time class
+
+        // Enable melee collider
+        meleeAttackCollider.enabled = true;
+
+        while (Time.time - startTime < meleeAttackDuration)
+        {
+            yield return false;
+        }
+
+        // Disable it after the duration
+        meleeAttackCollider.enabled = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        IHittable otherIHittable = collision.gameObject.GetComponent<IHittable>();
+        if (otherIHittable != null)
+        {
+            otherIHittable.OnHit(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/ReceiveDamage.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReceiveDamage : MonoBehaviour, IHittable
+{
+    public void OnHit(GameObject otherObject)
+    {
+        Debug.Log("[" + gameObject.name + "] Got hit by \"" + otherObject.name + "\"");
+    }
+}
